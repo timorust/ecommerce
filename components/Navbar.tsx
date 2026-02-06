9e1b27b13f25ac73fe5abe18e650fa8c1a040bff
Tamir Rustamov <timorust1@gmail.com>
2026-02-06 07:23:15 +0200

    cartCount

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { useCartStore } from "@/store/cart-store";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import React from "react";
 
 export default function Navbar() {
+  const { items } = useCartStore();
+
+  const cartCount = items.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
     <nav className="sticky top-0 z-50 bg-blue-50 shadow">
       <div className="container mx-auto flex items-center justify-between px-4 py-4">
@@ -17,7 +24,16 @@ export default function Navbar() {
             Checkout
           </Link>
         </div>
-        <div className="flex items-center space-x-4"></div>
+        <div className="flex items-center space-x-4">
+          <Link href="/checkout" className="relative">
+            <ShoppingCartIcon className="w-6 h-6" />
+            {cartCount > 0 && (
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+                {cartCount}
+              </span>
+            )}
+          </Link>
+        </div>
       </div>
     </nav>
   );
