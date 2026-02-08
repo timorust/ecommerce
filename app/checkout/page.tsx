c9365bce2c0e8bf483689ea5a7a4c80487aa66ba
Tamir Rustamov <timorust1@gmail.com>
2026-02-08 10:30:38 +0200

    checkoutAction

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,12 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useCartStore } from "@/store/cart-store";
+import { checkoutAction } from "./checkout-action";
 
 export default function CheckoutPage() {
   const { items, removeItem, addItem } = useCartStore();
   const total = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
-    0
+    0,
   );
 
   if (items.length === 0)
@@ -66,7 +67,10 @@ export default function CheckoutPage() {
         </CardContent>
       </Card>
 
-      <form action="" className="mt-4 border-t pt-2 text-lg font-semibold">
+      <form
+        action={checkoutAction}
+        className="mt-4 border-t pt-2 text-lg font-semibold"
+      >
         <input type="hidden" name="items" value={JSON.stringify(items)} />
         <Button type="submit" variant="default" className="w-full">
           Proceed to Payment
